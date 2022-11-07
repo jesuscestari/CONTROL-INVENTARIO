8dd45f6be4172607d2326bd233749c5543275f4a
Lolo <jescesta@gmail.com>
2022-11-07 19:50:33 -0300

    mensaje de error en login

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 // checking for minimum PHP version
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+    exit("PHP VERSION ERROR");
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
